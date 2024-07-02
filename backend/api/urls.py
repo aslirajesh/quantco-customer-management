57d26b3f5a3c7d80b20475776a4e3926d31b1a1b
aslirajesh <rajeshkumar9896@gmail.com>
2024-07-02 19:50:56 +0530

    backend-api

---
+++ backend/api/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from customers.views import CustomerViewSet
+
+
+router = DefaultRouter()
+router.register(r'customers', CustomerViewSet)
+
+
+
+urlpatterns = [
+    path('v1/', include(router.urls)),
+]
