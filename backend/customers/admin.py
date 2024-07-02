c0c2f14656588a4a231975865170e2af9cef8793
aslirajesh <rajeshkumar9896@gmail.com>
2024-07-02 20:34:22 +0530

    regidter in admin

--- backend/customers/admin.py
+++ backend/customers/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Customer
 
-# Register your models here.
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'date_of_birth', 'phone_number')
+
+admin.site.register(Customer, CustomerAdmin)
